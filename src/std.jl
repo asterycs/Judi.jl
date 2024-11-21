ec57312c4809e4e1e5c4e33e77d52b443ed6fa40
Jimmy Envall <jimmyenvall@gmail.com>
2024-11-21 17:42:55 +0100

    Register symbols (for use with API later)

--- src/std.jl
+++ src/std.jl
@@ -1,11 +1,20 @@
 export create_matrix
 export create_vector
 
+const global REGISTERED_SYMBOLS = Dict{String, Tensor}()
 
 function create_matrix(name::String)
-    return Tensor(name, Upper(get_next_letter()), Lower(get_next_letter()))
+    T = Tensor(name, Upper(get_next_letter()), Lower(get_next_letter()))
+
+    REGISTERED_SYMBOLS[name] = T
+
+    return T
 end
 
 function create_vector(name::String)
-    return Tensor(name, Upper(get_next_letter()))
+    T = Tensor(name, Upper(get_next_letter()))
+
+    REGISTERED_SYMBOLS[name] = T
+
+    return T
 end
